c3a65c3952c1c5cb034c1b5b3f9f04bf3dc81a2c
gkseogus <fbznffldj998@naver.com>
2022-02-14 11:58:04 +0900

    220214_주석업데이트

--- src/store/cart/reducer.ts
+++ src/store/cart/reducer.ts
@@ -50,17 +50,18 @@ const reducer: Reducer<cartState> = (state = initialState, action) => {
     // filter를 사용할 때는 items의 index로 구분하자
     case CartActionTypes.REMOVE_FROM_ITEM: {
       // console.log('ttt',action.payload.id)
+      // console.log('ttt',state.data.items)
       return {
         errors: state.errors,
         loading: state.loading,
         data: {
           ...state.data,
-          // findIndex를 통해 items의 id와 payload의 id가 같으면 그 요소의 첫번째 인덱스 값을 반환 (하나만 지우기 위함)
-             // ㄴ 즉 사용자가 클릭한 payload의 id와 store안의 여러 item중 같은 id가 있는지 확인하고 같으면 해당하는 첫번째 인덱스를 반환
+          // findIndex를 통해 사용자가 클릭한 payload의 id와 store안의 여러 item중 같은 id가 있는지 확인하고 같으면 
+          // state.items에서 해당하는 첫번째 인덱스를 반환
           // i(store에 있던 item의 index)가 findIndex로 얻은 payload의 인덱스 값과 다르면 filter 작동 x 
              // ㄴ 같으면 그 값만 배열의 요소에서 삭제  
-          // _k -> 단순히 number타입이랑 inventory 타입이랑 비교하기 위함 이때 k는 private 
-          items: state.data.items.filter((_k, i) => i !== state.data.items.findIndex(v => v.id === action.payload.id))
+          // 이때 v는 private 
+          items: state.data.items.filter((_v, i) => i !== state.data.items.findIndex(v => v.id === action.payload.id))
 
           // ex
           // 1. [0,1,2,3,4] 에서 0,1,4의 id가 같다고 가정하면 findIndex를 통해 0,1,4에서 0의 인덱스 값만 추출
