3c542af56bade13399a3e7c8709e7815157bc099
gkseogus <fbznffldj998@naver.com>
2022-02-14 15:47:21 +0900

    220214_주석업데이트

--- src/store/cart/reducer.ts
+++ src/store/cart/reducer.ts
@@ -57,7 +57,7 @@ const reducer: Reducer<cartState> = (state = initialState, action) => {
         data: {
           ...state.data,
           // findIndex를 통해 사용자가 클릭한 payload의 id와 store안의 여러 item중 같은 id가 있는지 확인하고 같으면 
-          // state.items에서 해당하는 첫번째 인덱스를 반환
+          // action.payload에서 해당하는 첫번째 인덱스를 반환
           // i(store에 있던 item의 index)가 findIndex로 얻은 payload의 인덱스 값과 다르면 filter 작동 x 
              // ㄴ 같으면 그 값만 배열의 요소에서 삭제  
           // filter는 state.data.items의 배열을 판별하고 그때 배열의 요소는 k
