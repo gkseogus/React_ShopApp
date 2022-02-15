dff7ee904992fcf6ffbf257cfb4941c323ae7b63
gkseogus <fbznffldj998@naver.com>
2022-02-15 11:54:44 +0900

    220215_creatItemType

--- src/store/inventory/types.ts
+++ src/store/inventory/types.ts
@@ -8,11 +8,14 @@ export interface Inventory {
   currentInventory: number;
 }
 
+export type InventoryCreate = Omit<Inventory, "id"> // id는 자동 생상할거라서 ID를 뺀값만 생성할때 사용
+
 // 전송 가능한 액션 유형
 export enum InventoryActionTypes {
   FETCH_REQUEST = "@@inventory/FETCH_REQUEST", // 요청
   FETCH_SUCCESS = "@@inventory/FETCH_SUCCESS", // 성공적인 응답
-  FETCH_ERROR = "@@inventory/FETCH_ERROR" // 에러
+  FETCH_ERROR = "@@inventory/FETCH_ERROR", // 에러
+  CREATE_ITEM = "@@inventory/CREATE_ITEM" // item생성
 }
 
 export interface InventoryState {
