3fc2c8c0afce27fd56d28b4066dfe3d4428aca2d
gkseogus <fbznffldj998@naver.com>
2022-02-20 19:51:48 +0900

    220220_주석추가

--- src/store/inventory/types.ts
+++ src/store/inventory/types.ts
@@ -11,7 +11,7 @@ export interface Inventory {
 // Omit: 생략, id는 자동 생상할거라서 ID를 뺀값만 생성할때 사용
 export type InventoryCreate = Omit<Inventory, "id"> 
 
-// 전송 가능한 액션 유형 
+// 전송 가능한 액션 유형 (type은 const로 하는게 좋다)
 export const FETCH_REQUEST = "@@inventory/FETCH_REQUEST"; // 요청
 export const FETCH_SUCCESS = "@@inventory/FETCH_SUCCESS"; // 성공적인 응답
 export const FETCH_ERROR = "@@inventory/FETCH_ERROR"; // 에러
