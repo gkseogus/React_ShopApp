a89c77b9a76316d5b71160bccfee91f4d0b0e27f
gkseogus <fbznffldj998@naver.com>
2022-02-20 19:47:56 +0900

    220220_type is const

--- src/store/inventory/types.ts
+++ src/store/inventory/types.ts
@@ -11,14 +11,12 @@ export interface Inventory {
 // Omit: 생략, id는 자동 생상할거라서 ID를 뺀값만 생성할때 사용
 export type InventoryCreate = Omit<Inventory, "id"> 
 
-// 전송 가능한 액션 유형
-export enum InventoryActionTypes {
-  FETCH_REQUEST = "@@inventory/FETCH_REQUEST", // 요청
-  FETCH_SUCCESS = "@@inventory/FETCH_SUCCESS", // 성공적인 응답
-  FETCH_ERROR = "@@inventory/FETCH_ERROR", // 에러
-  CREATE_ITEM = "@@inventory/CREATE_ITEM", // item생성
-  DELETE_ITEM = "@@inventory/DELETE_ITEM" // item삭제
-}
+// 전송 가능한 액션 유형 
+export const FETCH_REQUEST = "@@inventory/FETCH_REQUEST"; // 요청
+export const FETCH_SUCCESS = "@@inventory/FETCH_SUCCESS"; // 성공적인 응답
+export const FETCH_ERROR = "@@inventory/FETCH_ERROR"; // 에러
+export const CREATE_ITEM = "@@inventory/CREATE_ITEM"; // item생성
+export const DELETE_ITEM = "@@inventory/DELETE_ITEM";
 
 export interface InventoryState {
   readonly loading: boolean;
