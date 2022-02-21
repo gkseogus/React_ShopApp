ad7e61029f83539463782dd2159504677c499b76
gkseogus <fbznffldj998@naver.com>
2022-02-21 18:42:43 +0900

    220221_saga삭제

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,14 +3,9 @@ import { connectRouter } from "connected-react-router";
 
 import { History } from "history";
 
-// redux saga : 액션을 모니터링하고 있다가 특정 액션이 발생하면 이에 따라 특정 작업을 하는 방식으로 사용
-// 특정 작업 : js 실행, 액션을 디스패치, 현재 상태를 로드
-
-// import inventorySaga from "./inventory/sagas";
 import { InventoryReducer } from "./inventory/reducer";
 import { InventoryState } from "./inventory/types";
 
-// import cartSaga from "./cart/sagas";
 import { cartReducer } from "./cart/reducer";
 import { cartState } from "./cart/types";
 import { RouterState } from "connected-react-router";
