76c791d6c1d1be3dae69c9f96acf9201acbaf143
gkseogus <fbznffldj998@naver.com>
2022-02-21 18:55:44 +0900

    220221_리덕스 개발자 도구 사용

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import { routerMiddleware } from "connected-react-router";
 import { History } from "history";
 import { ApplicationState, createRootReducer } from "./store";
-import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
 // 여기서 configureStore는 단지 함수명이다. (모듈로 불러오지 않음)
 export default function configureStore(
@@ -12,12 +12,11 @@ export default function configureStore(
   // 컴포넌트들의 각 초기상태를 store에 저장
   initialState: ApplicationState 
 ): Store<ApplicationState> {
-  const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
     createRootReducer(history),
     initialState,
     // 리덕스는 액션 -> 미들웨어 -> 리듀서 -> 스토어 순으로 동작
-    applyMiddleware(routerMiddleware(history),sagaMiddleware, thunk)
+    composeWithDevTools(applyMiddleware(routerMiddleware(history), thunk))
   );
   return store;
 }
