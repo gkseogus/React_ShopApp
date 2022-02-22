60f25ec510fce5e3c04f5065c68073c8f9aaa89d
gkseogus <fbznffldj998@naver.com>
2022-02-23 00:05:53 +0900

    220223_ApiData action

--- src/store/inventory/action.ts
+++ src/store/inventory/action.ts
@@ -4,7 +4,6 @@ import { ActionCreator, Action, Dispatch } from "redux";
 import { ThunkAction } from "redux-thunk";
 
 import { ApplicationState } from "../index";
-import inventory from "../../mockdata";
 
 export type AppThunk = ActionCreator<
   ThunkAction<
@@ -16,18 +15,17 @@ export type AppThunk = ActionCreator<
 
 type NewType = ThunkAction<void, ApplicationState, Inventory, Action<string>>;
 
-// mockdata를 받아와 reducer에게 데이터를 전송
-export const fetchRequest: AppThunk = () => {
+// apiData를 받아와 reducer에게 데이터를 전송
+export const fetchRequest: AppThunk = (apiData) => {
   return (dispatch: Dispatch): Action => {
     try {
-      // 각 두개의 dispatch에 action을 담아 전송
-      return dispatch({ // 리듀서에게 inventory 데이터 전송
+      return dispatch({ // 리듀서에게 apiData 데이터 전송
         type: FETCH_SUCCESS,
-        payload: inventory // inventory 데이터를 전송 ( mockdata )
+        payload: apiData 
       });
     } catch (e) { 
       return dispatch({
-        type: FETCH_ERROR // 에러이면...
+        type: FETCH_ERROR
       });
     }
   };
