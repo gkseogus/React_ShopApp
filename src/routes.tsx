9ff9dedd18b5cbde0791d590b5f787ca366377e9
gkseogus <fbznffldj998@naver.com>
2022-02-24 14:58:57 +0900

    220224_route초기화

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,22 +4,17 @@ import { Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import Cart from "./components/Cart";
-import Googlebutton from "./components/GoogleLogin";
-
 
 const Routes: React.SFC = () => (
   <div>
       {/* 첫 번째 route */}
       <Route
-
         exact
         path="/" 
         render={() => ( 
-          // <Googlebutton>
           <Navbar>
             <HomePage/> 
           </Navbar>
-          // </Googlebutton>
         )}
       />
       {/* 두 번째 route */}
