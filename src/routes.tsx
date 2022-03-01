6ac128f735dfe9f05070993bf4fff946d832b1b8
gkseogus <fbznffldj998@naver.com>
2022-03-01 23:30:34 +0900

    220301_라우터 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,38 +1,38 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
-import HomePage from "./components/HomePage";
-import Cart from "./components/Cart";
-import Googlebutton from "./components/GoogleLogin";
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Cart from './components/Cart';
+import Googlebutton from './components/GoogleLogin';
+import AuthController from './AuthController';
 
 const Routes: React.SFC = () => (
   <div>
-      {/* 첫 번째 route */}
-      <Route
-        exact
-        path="/" 
-        render={() => ( 
-          <Navbar>
-            <HomePage/>
-          </Navbar>
-        )}
-      />
-      {/* 두 번째 route */}
-      <Route
-        path="/cart" 
-        render={() => ( 
-          <Navbar>
-            <Cart />
-          </Navbar>
-        )}
-      />
-      <Route 
-        path="/GoogleLogin" 
-        render={() => ( 
-          <Googlebutton/>
-        )}
-      />
+    <AuthController />
+    {/* 첫 번째 route */}
+    <Route
+      exact
+      path="/"
+      render={() => (
+        <Navbar>
+          <HomePage />
+        </Navbar>
+      )}
+    />
+    {/* 두 번째 route */}
+    <Route
+      path="/cart"
+      render={() => (
+        <Navbar>
+          <Cart />
+        </Navbar>
+      )}
+    />
+    <Route path="/GoogleLogin" 
+      render={() => 
+        <Googlebutton />
+        } />
   </div>
 );
 
