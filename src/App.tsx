9b5e30f890b076241efde4db9c90426aeb20b659
gkseogus <fbznffldj998@naver.com>
2022-08-11 13:58:31 +0900

    220811_app

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { ApplicationState } from "./store";
 import { Store } from "redux";
 import { History } from "history";
 import { ConnectedRouter } from "connected-react-router";
-import Routes from "./routes";
+import AllRoutes from "./routes";
 interface MainProps {
   store: Store<ApplicationState>; // 현재 상태를 store에 저장
   history: History; // 현재까지 이동한 url 경로들이 담겨있다.
@@ -20,7 +20,7 @@ const App: React.FC<MainProps> = ({ store, history }) => {
     <Provider store={store}> 
       <ThemeProvider theme={customTheme}>
         <ConnectedRouter history={history}>
-            <Routes />
+            <AllRoutes />
         </ConnectedRouter>
       </ThemeProvider>
     </Provider>
